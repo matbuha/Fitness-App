faa50f4f1b7b746d427d500c34d21f497d3ab22b
ariel <95013498+matbuha@users.noreply.github.com>
2025-02-23 22:52:16 +0200

    adding session checks

--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@ if (!token) {
   window.location.href = "auth.html";
 }
 
+
 // יצירת לקוח Supabase (החלף את המפתח במפתח המתאים)
 const supabaseUrl = "https://kmwvlpganjabpcqsnrkx.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttd3ZscGdhbmphYnBjcXNucmt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4NzU1MjgsImV4cCI6MjA1NTQ1MTUyOH0.MZhYAWfFI7YTDde44SIhZfSovqCT8DYAZbgtRw7nOEs";
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
+
+async function checkSession() {
+  const { data, error } = await supabaseClient.auth.getSession();
+  console.log("Current session:", data.session);
+}
+checkSession();
 
 // --- פונקציות CRUD ---
 
